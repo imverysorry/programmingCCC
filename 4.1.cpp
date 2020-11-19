adaae7b03c5cc7ee880c0c94040ac70cdd082f00
imverysorry <74419197+imverysorry@users.noreply.github.com>
2020-11-19 10:25:45 +0300

    Add files via upload

---
+++ 4.1.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>;
+int main()
+{
+    int a[5];
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        scanf_s("%d", &a[i]);
+    }
+printf("%d %d %d %d %d\n", a[0], a[1], a[2], a[3], a[4]);
+}
